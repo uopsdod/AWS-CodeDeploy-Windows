c3eb70cb37caa65acb2f996868dd203a064c4e9b
stsai <stsai@VDIGTPWDLC0024.na.webmd.net>
2018-11-19 11:18:49 +0800

    first commit

---
+++ src/main/java/AWS_CodeDeploy_Windows/AWS_CodeDeploy_Windows/App.java
@@ -0,0 +1,13 @@
+package AWS_CodeDeploy_Windows.AWS_CodeDeploy_Windows;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
